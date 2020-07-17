3bdc317bfaadb7a9aed57086c548f66d1b0f2d61
Ethan Falicov <t-etfali@microsoft.com>
2020-07-16 18:11:54 -0700

    Adding better command line argument handling to the bare-bones extension

--- main.go
+++ main.go
@@ -27,29 +27,32 @@ func update() {
 }
 
 func main() {
-	for _, a := range os.Args[1:] {
+	if len(os.Args[1:]) > 0 {
+		for _, a := range os.Args[1:] {
+			/* 	TODO : Not sure if there is a better method in regexp so don't need multiple vars
+			 */
+			matchDisable, _ := regexp.MatchString("^([-/]*)(disable)", a)
+			matchUninstall, _ := regexp.MatchString("^([-/]*)(uninstall)", a)
+			matchInstall, _ := regexp.MatchString("^([-/]*)(install)", a)
+			matchEnable, _ := regexp.MatchString("^([-/]*)(enable)", a)
+			matchUpdate, _ := regexp.MatchString("^([-/]*)(update)", a)
 
-		/* 	TODO : Not sure if there is a better method in regexp so don't need multiple vars
-		 */
-		matchDisable, _ := regexp.MatchString("^([-/]*)(disable)", a)
-		matchUninstall, _ := regexp.MatchString("^([-/]*)(uninstall)", a)
-		matchInstall, _ := regexp.MatchString("^([-/]*)(install)", a)
-		matchEnable, _ := regexp.MatchString("^([-/]*)(enable)", a)
-		matchUpdate, _ := regexp.MatchString("^([-/]*)(update)", a)
-
-		if matchDisable {
-			disable()
-		} else if matchUninstall {
-			uninstall()
-		} else if matchInstall {
-			install()
-		} else if matchEnable {
-			enable()
-		} else if matchUpdate {
-			update()
-		} else {
-			fmt.Println("Command Not Recognized.")
+			if matchDisable {
+				disable()
+			} else if matchUninstall {
+				uninstall()
+			} else if matchInstall {
+				install()
+			} else if matchEnable {
+				enable()
+			} else if matchUpdate {
+				update()
+			} else {
+				fmt.Println("Command Not Recognized.")
+			}
 		}
+	} else {
+		fmt.Println("No command line arguments provided")
 	}
 	/* 	TODO : Error handling might be necessary for if there is no match, but this could
 	just be a print statement else case if the regexp doesn't raise panics/errors
